66a2c0edcb83b2e382f81a3ede672444006463b1
geronda94 <iiiliev.igor@gmail.com>
2023-10-22 21:53:54 +0300

    1234

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
+from datetime import datetime
+from enum import Enum
 import time
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import List, Optional, Union
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
 import asyncio
 from db import db
+
+
+
 
 app = FastAPI(
     title='Trading app'
 )
 
 # uvicorn main:app --reload --host 192.168.205.217 --port 8000
+
+
+@app.exception_handler(ValidationException)
+async def validation_except_handler(request: Request, exc: ValidationException):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({'detail':exc.errors()}),
+    )
+
+
+
+
 
 class Item(BaseModel):
     name: str
@@ -18,13 +38,35 @@ class Item(BaseModel):
 
 
 fake_users = [
-    {'id':1,'role':'admin', 'name':'Bob'},
+    {'id':1,'role':'admin', 'name':['Bob']},
     {'id':2,'role':'investor', 'name':'Alex'},
     {'id':3,'role':'trader', 'name':'Jhon'},
+    {'id':4,'role':'investor', 'name':'Homer', 'degree':[
+        {'id':1, 'created_at':'2020-01-01T00:00:00', 'type_degree':'newbie'}
+    ]}
 ]
 
 
-@app.get('/users/{user_id}')
+
+class DegreeType(Enum):
+    newbie ='newbie'
+    expert ='expert'
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] =[]
+
+
+@app.get('/users/{user_id}', response_model=List[User])
 def get_user(user_id: int):
     return [x for x in fake_users if x.get('id') == user_id]
 
@@ -56,6 +98,20 @@ def change_user_name(user_id: int, new_name: str):
 
 
 
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.post('/trades')
+def add_trades(trades: list[Trade]):
+    fake_trades.extend(trades)
+    return {'status':200, 'data':fake_trades}
 
 
 
@@ -90,17 +146,18 @@ async def get_fake():
 
 
 
-@app.on_event("startup")
+# Обработчик события "startup"
 async def startup_event():
     await db.connect()
     asyncio.create_task(run_periodically())
 
-
-# Отключение от базы данных при завершении работы приложения
-@app.on_event("shutdown")
+# Обработчик события "shutdown"
 async def shutdown_event():
     await db.disconnect()
 
+# Регистрация обработчиков событий жизненного цикла с помощью LifespanManager
+app.add_event_handler("startup", startup_event)
+app.add_event_handler("shutdown", shutdown_event)
 
 async def open_and_close_connection():
     await db.disconnect()
