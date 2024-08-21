99d8f1e0bacd937f2f37ac9270e90ac5edfbce33
geronda94 <iiiliev.igor@gmail.com>
2024-08-21 23:23:40 +0300

    2662

--- src/operations/router.py
+++ src/operations/router.py
@@ -2,11 +2,11 @@ import datetime
 from typing import List, Type
 from pydantic import BaseModel
 from sqlalchemy import insert, select
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from database import AsyncSession, get_async_session
 from operations.models import operation
 from operations.schemas import OperationCreate, OperationModel
-
+from typing import Optional, Any
 
 router = APIRouter(
     prefix='/operations',
@@ -14,14 +14,30 @@ router = APIRouter(
 )
 
 
+class ResponseModel(BaseModel):
+    status: str
+    data: Optional[List[OperationModel]] = None
+    details: Optional[str] = None
 
 
-@router.get('/}', response_model=List[OperationModel])
-async def get_specific_operation(operation_type:None, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == 'sell')
-    result = await session.execute(query)
-    res = result.all()
-    return res
+@router.get('/}', response_model=ResponseModel)
+async def get_specific_operation(operation_type, session: AsyncSession = Depends(get_async_session)):
+    try:
+        query = select(operation).where(operation.c.type == 'sell')
+        result = await session.execute(query)
+        res = result.mappings().all()
+        return ResponseModel(
+            status='success',
+            data=res
+        )
+        
+
+    except Exception as ex:
+        raise HTTPException(
+            status_code=500,
+            detail=str(ex)
+        )
+        
         
         
 @router.post('/')
