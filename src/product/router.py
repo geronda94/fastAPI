7e958205b55e7df8185966085783ae3f90fae708
geronda94 <iiiliev.igor@gmail.com>
2024-11-20 23:37:53 +0200

    123

--- src/product/router.py
+++ src/product/router.py
@@ -5,17 +5,47 @@ from .models import Product
 from .schemas import ProductCreate, ProductRead
 from database import get_async_session
 
+from .models import Category, Product
+from .schemas import CategoryRead, ProductCreate
+from typing import List
+
 router = APIRouter(
                     prefix='/products',
                     tags=['Products']
                     )
 
 
-@router.post("/create-product", response_model=ProductRead, status_code=201)
-async def create_product(product: ProductCreate, session: AsyncSession = Depends(get_async_session)):
-    db_product = Product(**product.dict())
-    session.add(db_product)
-    await session.commit()
-    await session.refresh(db_product)
-    return db_product
 
+@router.post("/add-product")
+async def add_product(product_data: ProductCreate, db: AsyncSession = Depends(get_async_session)):
+    # Проверка на существование категории
+    category = await db.execute(select(Category).where(Category.id == product_data.category_id))
+    category = category.scalar_one_or_none()
+    if not category:
+        raise HTTPException(status_code=404, detail="Category not found")
+    
+    # Создание нового продукта
+    new_product = Product(**product_data.dict())
+    
+    # Сохранение в базе данных
+    try:
+        db.add(new_product)
+        await db.commit()
+        await db.refresh(new_product)
+    except Exception as ex:
+        print(ex)
+    
+    return {"message": "Product added successfully", "product": new_product}
+
+
+
+
+@router.get("/get-categories", response_model=List[CategoryRead])
+async def get_all_categories(session: AsyncSession = Depends(get_async_session)):
+    """
+    Возвращает все категории.
+    """
+    query = select(Category)
+    result = await session.execute(query)
+    categories = result.scalars().all()
+    return categories
--- src/product/schemas.py
+++ src/product/schemas.py
@@ -2,7 +2,20 @@ from pydantic import BaseModel, Field
 from typing import Optional, List
 
 
-class ProductBase(BaseModel):
+class BaseCategory(BaseModel):
+    title_en: str
+    title_ru: str
+    title_ua: str
+    title_tr: str
+    description_en: Optional[str]
+    description_ru: Optional[str]
+    description_ua: Optional[str]
+    description_tr: Optional[str]
+    is_available: Optional[bool] = True
+    code: Optional[str]
+
+
+class BaseProduct(BaseModel):
     title_en: str
     title_ru: Optional[str]
     title_ua: Optional[str]
@@ -16,20 +29,209 @@ class ProductBase(BaseModel):
     price: int
     sale: Optional[bool] = False
     discount_value: Optional[int]
+
+
+class BaseProductColor(BaseModel):
+    product_id: int
+    color_id: int
     avatar: Optional[str]
     slides: Optional[str]
     video: Optional[str]
-    category_id: int
+    is_available: Optional[bool] = True
 
 
-class ProductCreate(ProductBase):
-    pass
+class BaseSize(BaseModel):
+    value: str
 
 
-class ProductRead(ProductBase):
+class BaseProductSize(BaseModel):
+    product_color_id: int
+    size_id: int
+    quantity: int
+
+
+class BaseOrder(BaseModel):
+    name: str
+    email: str
+    phone: str
+    order_status: str
+    country: Optional[str]
+    city: Optional[str]
+    address: Optional[str]
+    delivery_method: str
+    payment_method: str
+    total_price: int
+
+
+class BaseOrderItem(BaseModel):
+    product_id: int
+    product_color_id: int
+    product_syze_id: int
+    quantity: int
+    price_per_unit: int
+    order_id: int
+
+
+
+
+
+
+class CategoryRead(BaseCategory):
     id: int
 
     class Config:
         orm_mode = True
 
 
+class ProductRead(BaseProduct):
+    id: int
+    category_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class ProductColorRead(BaseProductColor):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class SizeRead(BaseSize):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class ProductSizeRead(BaseProductSize):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class OrderRead(BaseOrder):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class OrderItemRead(BaseOrderItem):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+
+
+
+
+
+class CategoryCreate(BaseCategory):
+    pass
+
+
+class ProductCreate(BaseProduct):
+    category_id: int
+
+
+class ProductColorCreate(BaseProductColor):
+    pass
+
+
+class SizeCreate(BaseSize):
+    pass
+
+
+class ProductSizeCreate(BaseProductSize):
+    pass
+
+
+class OrderCreate(BaseOrder):
+    pass
+
+
+class OrderItemCreate(BaseOrderItem):
+    pass
+
+
+
+
+
+
+
+
+
+class CategoryUpdate(BaseCategory):
+    title_en: Optional[str]
+    title_ru: Optional[str]
+    title_ua: Optional[str]
+    title_tr: Optional[str]
+    description_en: Optional[str]
+    description_ru: Optional[str]
+    description_ua: Optional[str]
+    description_tr: Optional[str]
+    is_available: Optional[bool]
+    code: Optional[str]
+
+
+class ProductUpdate(BaseProduct):
+    title_en: Optional[str]
+    title_ru: Optional[str]
+    title_ua: Optional[str]
+    title_tr: Optional[str]
+    is_available: Optional[bool]
+    code: Optional[str]
+    description_en: Optional[str]
+    description_ru: Optional[str]
+    description_ua: Optional[str]
+    description_tr: Optional[str]
+    price: Optional[int]
+    sale: Optional[bool]
+    discount_value: Optional[int]
+    category_id: Optional[int]
+
+
+class ProductColorUpdate(BaseProductColor):
+    product_id: Optional[int]
+    color_id: Optional[int]
+    avatar: Optional[str]
+    slides: Optional[str]
+    video: Optional[str]
+    is_available: Optional[bool]
+
+
+class SizeUpdate(BaseSize):
+    value: Optional[str]
+
+
+class ProductSizeUpdate(BaseProductSize):
+    product_color_id: Optional[int]
+    size_id: Optional[int]
+    quantity: Optional[int]
+
+
+class OrderUpdate(BaseOrder):
+    name: Optional[str]
+    email: Optional[str]
+    phone: Optional[str]
+    order_status: Optional[str]
+    country: Optional[str]
+    city: Optional[str]
+    address: Optional[str]
+    delivery_method: Optional[str]
+    payment_method: Optional[str]
+    total_price: Optional[int]
+
+
+class OrderItemUpdate(BaseOrderItem):
+    product_id: Optional[int]
+    product_color_id: Optional[int]
+    product_syze_id: Optional[int]
+    quantity: Optional[int]
+    price_per_unit: Optional[int]
+    order_id: Optional[int]
